e856b3745f2d235407f8b9b58c7e5803e73440e9
Fatma Nur <fatmanurtml@gmail.com>
2022-01-25 16:33:25 +0300

    use if-else blocks

---
+++ Conditions/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int sayi;
+        sayi = 19;
+        //sayi = 20;
+
+        if(sayi<20){
+            System.out.println("Sayı 20 den küçüktür.");
+        }
+        else if(sayi>20){
+            System.out.println("Sayı 20 den büyüktür");
+        }
+        else{
+            System.out.println("Sayı 20 ye eşittir.");
+        }
+    }
+}
