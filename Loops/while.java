82e3a782525d93767a600874e773e7420ad9590f
Fatma Nur <fatmanurtml@gmail.com>
2022-01-26 23:54:40 +0300

    add while loop

---
+++ Loops/while.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //sayaç tanımlamasını döngünün dışında yapıyoruz
+        int i = 1;
+
+        //1-10 arasındaki sayıların ekrana yazdırılması
+        while (i<=10) //döngünün koşulunu belirtiyoruz
+        {
+            System.out.println(i);
+            i++; //sayacın artış miktarını belirtiyoruz
+        }
+    }
+}
