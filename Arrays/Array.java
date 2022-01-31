50f8bcadf0e512536bdc8a577345cfd49730f241
Fatma Nur <fatmanurtml@gmail.com>
2022-01-31 12:04:16 +0300

    add array in java

---
+++ Arrays/Array.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //değişken tipinin hemen sağ tarafına [] koyulur.
+        //atamanın sağ tarafına dizinin boyutu belirtilir.
+        //indeksler 0'dan başlar.
+
+        String[] ogrenciler = new String[4];
+        ogrenciler[0]="Fatmanur";
+        ogrenciler[1]="Kerim";
+        ogrenciler[2]="Engin";
+        ogrenciler[3]="Derin";
+
+        /*for(int i=0; i<ogrenciler.length; i++){
+            System.out.println(ogrenciler[i]);
+        }*/
+
+        //for döngüsü ile yapılan işlemin aynısını for each döngüsü ile daha kolay yapabilriz.
+        for (String ogreci:ogrenciler) {
+            System.out.println(ogreci);
+        }
+    }
+}
